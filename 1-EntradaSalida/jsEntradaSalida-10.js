a654629aeadeddadc3fd370bd4186a4823f6a13f
ignaciotarelli <60760329+ignaciotarelli@users.noreply.github.com>
2020-02-07 22:14:48 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+    var descuento;
+
+    importe = parseFloat(document.getElementById("importe").value);
+    descuento = importe *0.75;
+    document.getElementById("resultado").value = descuento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var sueldo
+var aumento
+var sueldoConAumento
+
+sueldo = parseInt(document.getElementById("sueldo").value);
+aumento = sueldo *0.1;
+sueldoConAumento = sueldo + aumento;
+document.getElementById("resultado").value = sueldoConAumento;
+
+
+
 
 	
 }
