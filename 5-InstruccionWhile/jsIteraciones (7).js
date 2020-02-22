09f4cc719e483af4f9269f2e7ac119aa53cea94d
ignaciotarelli <60760329+ignaciotarelli@users.noreply.github.com>
2020-02-21 21:02:41 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,30 @@
 function mostrar()
 {
-
+    var numero;
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var seguir='si';
+    var promedio;
 
+	
+do{
+	numero = parseInt(prompt("ingrese un numero: "));
+
+
+	while (isNaN(numero)){
+		numero = parseInt(prompt("Eso no es un numero. Ingrese un numero: "));
+
+	} 
+	acumulador = acumulador + numero;
+	contador ++;
+	
+	seguir = prompt("Quiere ingresar otro numero?");
+
+} while (seguir == 'si');
+
+promedio = acumulador / contador;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,35 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
 
+	do{
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		numero = parseInt(prompt("Ingrese un numero: "));
+		while (isNaN(numero)){
+			numero = parseInt(prompt("No es un numero. Ingrese un numero: "));
+		} 
 
-}//FIN DE LA FUNCIÓN
+		if (numero >= 0){
+			positivo = positivo + numero;
+			
+
+		} else{
+			negativo = negativo * numero;
+            contador=1
+		} 
+		seguir = prompt("Quiere ingresar otro numero?");
+		} while(seguir == 'si')
+
+		if(contador == 0){
+			negativo = 0;
+		}
+
+		document.getElementById('suma').value=positivo;
+		document.getElementById('producto').value=negativo;
+	}
+
+
+
+//FIN DE LA FUNCIÓN
